692b10e2b738bc154ca7c2c6335db910141187ba
AWSLxFBK <47930806+AWSLxFBK@users.noreply.github.com>
2019-02-23 20:16:08 -0700

    Add files via upload

---
+++ SnakePosition.java
@@ -0,0 +1,17 @@
+
+public class SnakePosition {
+
+    public int col;
+    public int row;
+
+    SnakePos(int row, int col) {
+        this.col = col;
+        this.row = row;
+    }
+
+    SnakePos() {
+        col = 0;
+        row = 0;
+    }
+
+}
