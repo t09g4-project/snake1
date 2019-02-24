88cb02466f9478d7cc7086dc90a1932d325695ca
AWSLxFBK <47930806+AWSLxFBK@users.noreply.github.com>
2019-02-23 20:25:20 -0700

    Update SnakePosition.java

--- SnakePosition.java
+++ SnakePosition.java
@@ -1,14 +1,14 @@
-
+//represent the single unit of the snake body
 public class SnakePosition {
 
     public int col;
     public int row;
-
+//constructor
     SnakePosition(int row, int col) {
         this.col = col;
         this.row = row;
     }
-
+//constructor
     SnakePosition() {
         col = 0;
         row = 0;
